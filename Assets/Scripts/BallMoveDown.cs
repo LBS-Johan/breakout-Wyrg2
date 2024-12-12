be3a2bc2cc837159d0f4fcf48a8b4bf253b4d675
Wyrg2 <felix.rosenkron@elev.ga.lbs.se>
2024-12-12 12:51:50 +0100

    stuff

--- Assets/Scripts/BallMoveDown.cs
+++ Assets/Scripts/BallMoveDown.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class BallMoveDown : MonoBehaviour
 {
-    int breakableAmount = 36;
+    public int breakableAmount = 36;
     public float ballSpeed;
     Rigidbody2D rb;
     // Start is called before the first frame update
